5f2b22f37f8f21c4ea6687e7e46511fcc9c62858
SammyD007 <52652279+SammyD007@users.noreply.github.com>
2023-08-14 19:34:21 -0700

    initial commit

---
+++ LFG_App/flask_app/controllers/LFGController.py
@@ -0,0 +1,91 @@
+from flask_app import app
+from flask import render_template, redirect, request, session, flash
+from flask_app.models.login_model import User
+from flask_app.models.game_model import Game
+from flask_bcrypt import Bcrypt
+
+bcrypt = Bcrypt(app)
+
+@app.route('/')
+def home():
+    return render_template('login_register.html', users = User.get_all_users())
+
+@app.route('/register', methods = ['POST'])
+def register():
+    if not User.validate_user(request.form): #lil weird but, this is a dubl negative technically, if it returns false then it is false for being "not" so it's true
+        return redirect('/')
+    print('****REQUEST FORM****', request.form)
+    pw_hash = bcrypt.generate_password_hash(request.form['password'])
+    print(pw_hash)
+    data = {
+        'first_name': request.form['first_name'],
+        'last_name': request.form['last_name'],
+        'email': request.form['email'],
+        'password': pw_hash
+    }
+    
+    user_id = User.create_user(data)
+    session['user_id'] = user_id
+    session['user_name'] = request.form['first_name']
+    
+    print('*********', session)
+    print('*********', pw_hash)
+    print('*********', data)
+    
+    return redirect('/welcome')
+
+@app.route('/login', methods = ["POST"])
+def login():
+    user = User.validate_login(request.form)
+    session['user_name'] = user.first_name
+    if not user:
+        flash('Invalid Email or Login')
+        return redirect('/')
+    if not bcrypt.check_password_hash(user.password, request.form['password']):
+        flash("Invalid Password","login")
+        return redirect('/')
+    return redirect('/welcome')
+
+@app.route('/connect')
+def connect_page():
+    return render_template('connect.html')
+
+@app.route('/welcome')
+def welcome():
+    user_name = session.get('user_name')
+    games = Game.get_all_games()
+    return render_template('welcome.html', user_name = user_name, games = games)
+
+@app.route('/edit')
+def edit():
+    return render_template('edit.html')
+
+@app.route('/logout')
+def logout():
+    session.clear
+    return render_template('login_register.html')
+
+@app.route('/results/<int:game_id>')
+def get_results(game_id):
+    user_has_games = Game.get_results(game_id)
+    return render_template('results.html', user_has_games=user_has_games, game_id=game_id)
+
+@app.route('/profile')
+def profile_page():
+    user_id = session.get('user_id')
+    games = Game.get_all_games()
+    return render_template('profile_page.html', games = games, user_id = user_id)
+
+@app.route('/add_game', methods=['POST'])
+def add_game():
+    user_id = session.get('user_id')
+    game_name = request.form['game_name']
+
+    # Get the game_id by querying the games table
+    game_id = Game.get_game_id_by_name(game_name)
+
+    # Add the user-game relationship to the user_has_games table
+    if game_id:
+        Game.add_user_game_relation(user_id, game_id)
+    
+    return redirect('/profile')
---
+++ LFG_App/flask_app/models/game_model.py
@@ -0,0 +1,81 @@
+from flask_app import app
+from flask_app.config.mysqlconnection import connectToMySQL
+from flask import flash, request, session
+
+db = 'LFG_app_schema'
+
+class Game:
+    def __init__(self, data):
+        self.id = data['id']
+        self.name = data['name']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+    
+    @classmethod
+    def submit_game(cls, data, user_id):
+        query = '''INSERT INTO games (name, user_id)
+        VALUES (%(name)s, %(user_id)s);'''
+    
+        data['user_id'] = user_id
+        db_response = connectToMySQL(db).query_db(query, data)
+        return db_response
+    
+    @classmethod
+    def get_all_games(cls):
+        query = '''SELECT * FROM games;'''
+        db_response = connectToMySQL(db).query_db(query)
+        return db_response
+    
+    @classmethod
+    def get_game_id_by_name(cls, game_name):
+        query = 'SELECT id FROM games WHERE name = %(game_name)s;'
+        game_data = {'game_name': game_name}
+        db_response = connectToMySQL(db).query_db(query, game_data)
+        if db_response:
+            return db_response[0]['id']
+        return None
+
+    @classmethod
+    def add_user_game_relation(cls, user_id, game_name):
+        game_query = '''SELECT id FROM games WHERE name = %(game_name)s;'''  # Retrieve the game's id
+        game_data = {'game_name': game_name}
+        game_result = connectToMySQL(db).query_db(game_query, game_data)
+        
+        if not game_result:
+            return None  # Return None if the game is not found
+
+        game_id = game_result[0]['id']
+
+        query = '''INSERT INTO user_has_games (user_id, game_id) VALUES (%(user_id)s, %(game_id)s);'''
+        user_game_data = {'user_id': user_id, 'game_id': game_id}
+        db_response = connectToMySQL(db).query_db(query, user_game_data)
+
+        return db_response
+    
+    @classmethod
+    def get_results(cls, game_id):
+        query = '''SELECT users.first_name AS user_name, games.name AS game_name
+        FROM users
+        JOIN user_has_games ON users.id = user_has_games.user_id
+        JOIN games ON user_has_games.game_id = games.id
+        WHERE games.id = %(game_id)s;'''
+        db_response = connectToMySQL(db).query_db(query, {'game_id': game_id})
+        return db_response
+    
+    @classmethod
+    def add_game(cls, user_id, game_name):
+        game_query = '''SELECT id FROM games WHERE name = %(game_name)s;'''
+        game_data = {'game_name': game_name}
+        game_result = connectToMySQL(db).query_db(game_query, game_data)
+        
+        if not game_result:
+            print(f"Game '{game_name}' not found.")
+            return
+        
+        game_id = game_result[0]['id']
+        
+        query = '''INSERT INTO user_has_games (user_id, game_id) VALUES (%(user_id)s, %(game_id)s);'''
+        user_game_data = {'user_id': user_id, 'game_id': game_id}
+        db_response = connectToMySQL(db).query_db(query, user_game_data)
+        
+        return db_response
